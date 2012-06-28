fa494f762272a2d66c8e40632c025500807b7e9a
Elizabeth Garbee <egarbee@cyberpointllc.com>
2012-06-28 13:47:11 -0400

    part 1 of the EM algorithm written (kmeans) - before troubleshooting.

--- EM_Algorithm.cpp
+++ EM_Algorithm.cpp
@@ -17,38 +17,72 @@ using namespace std;
 
 // function that reads and parses a comma (or tab) delineated file
 void ReadCSV(vector<string> &record, const string& line, char delimiter);
+void kmeans(int dim, double *X, int n, int k, double *cluster_centroid, int *cluster_assignment_final);
+vector<double> * ParseCSV(int argc, char *argv[]);
 
 /*************************************************************************************************************/
 /** MAIN **
 **************************************************************************************************************/
 int main(int argc, char *argv[])
 {
-	// read and parse the CSV data
-	cout << "This algorithm takes mixture data in a CSV. Make sure to replace test_data.txt with your file name. \n"; 
-	vector<string> row;
-	string line;
-	ifstream in("test_data.txt");
-	if (in.fail())
+	int i;
+
+	vector<double> csv_data = *ParseCSV(argc, argv);
+	int n = csv_data.size();
+	double * stored_data = new double[n];
+	for (i = 0; i < n; i++)
 	{
-		cout << "File not found" << endl;
-		return 0;
+		stored_data[i] = csv_data[i];
 	}
-	while (getline(in, line) && in.good())
-	{
-		ReadCSV(row, line, ',');
-		for (int i = 0, leng = row.size(); i < leng; i++)
-		{
-			cout << row[i] << "\t";
-		}
-		cout << endl;
-	}
-	in.close();
-	return 0;
+	// needs to be changed for specific use
+	int dim = 1;
+	// this too
+	int k = 3;
+	double *cluster_centroid = new double[k];
+	int *cluster_assignment_final = new int[n];
+
+	// perfom a KMeans analysis to determine initial cluster means for the EM algorithm to use
+	kmeans(dim, stored_data, n, k, cluster_centroid, cluster_assignment_final);
+
+	delete[] stored_data;
+	delete[] cluster_centroid;
+	delete[] cluster_assignment_final;
 }
 
 /*************************************************************************************************************/
 /** FUNCTIONS **
 **************************************************************************************************************/
+vector<double> * ParseCSV(int argc, char *argv[])
+{
+	// read and parse the CSV data
+		cout << "This algorithm takes mixture data in a CSV. Make sure to replace test_data.txt with your file name. \n";
+		vector<string> row;
+		vector<double> *data = new vector<double>();
+		int line_number = 0;
+		string line;
+		ifstream in("test_data.txt");
+		if (in.fail())
+		{
+			cout << "File not found" << endl;
+			return 0;
+		}
+		while (getline(in, line) && in.good())
+		{
+			ReadCSV(row, line, ',');
+			// only looking at row[0] now because of an assumption of 1D data
+			// count through line_number
+			const char* s = row[0].c_str();
+			data->push_back(atof(s));
+			// now the data is stored in a vector of doubles called data
+			//for (int i = 0, leng = row.size(); i < leng; i++)
+			//{
+				//cout << row[i] << "\t";
+			//}
+			//cout << endl;
+		}
+		in.close();
+		return data;
+}
 
 void ReadCSV(vector<string> &record, const string& line, char delimiter)
 {
@@ -186,8 +220,8 @@ void calc_cluster_centroids(int dim, int n, int k, double *X, int *cluster_assig
 	// initialize cluster centroid coordinate sums to zero
 	for (int ii = 0; ii < k; ii++)
 	{
-		cluster_member_count = 0;
-		for (int jj = 0; jj < dim; j++)
+		cluster_member_count[ii] = 0;
+		for (int jj = 0; jj < dim; jj++)
 			new_cluster_centroid[ii*dim + jj] = 0;
 	}
 	// sum all points for every point
@@ -240,7 +274,7 @@ void update_delta_score_table(int dim, int n, int k, double *X, int *cluster_ass
 	}
 }
 
-void perform_move (int dim, int n, int k, double *X, int *cluster_assignment_cur, double *cluster_centroid, int *cluster_member_count, int move_point, int move_target_cluster)
+void perform_move (int dim, int n, int k, double *X, int *cluster_assignment, double *cluster_centroid, int *cluster_member_count, int move_point, int move_target_cluster)
 {
 	int cluster_old = cluster_assignment[move_point];
 	int cluster_new = move_target_cluster;
@@ -251,7 +285,7 @@ void perform_move (int dim, int n, int k, double *X, int *cluster_assignment_cur
 	cluster_member_count[cluster_new]++;
 
 	if (cluster_member_count[cluster_old] <= 1)
-		cout << "Warning! can't handle single-member clusters \n" << endl
+		cout << "Warning! can't handle single-member clusters \n" << endl;
 	// update centroid array
 	for (int ii = 0; ii < dim; ii++)
 	{	
@@ -260,15 +294,14 @@ void perform_move (int dim, int n, int k, double *X, int *cluster_assignment_cur
 	}
 }
 
-/* void cluster_diag(int dim, int n, int k, double *X, int *cluster_assignment_index, double *cluster_centroid)
+void cluster_diag(int dim, int n, int k, double *X, int *cluster_assignment_index, double *cluster_centroid)
 {
 	int cluster_member_count[MAX_CLUSTERS];
 	get_cluster_member_count(n, k, cluster_assignment_index, cluster_member_count);
 	cout << "  Final clusters \n" << endl;
 	for (int ii = 0; ii < k; ii++)
-		cout << "   cluster
-
-} */
+		printf("   cluster %d:       members: %8d, centroid(%.1f) \n", ii, cluster_member_count[ii], cluster_centroid[ii*dim + 0]/*, cluster_centroid[ii*dim  + 1]*/);
+} 
 
 void copy_assignment_array(int n, int *src, int *tgt)
 {
@@ -294,17 +327,18 @@ void kmeans(int dim, double *X, int n, int k, double *cluster_centroid, int *clu
 // int *cluster_assignment_final = output
 
 {
-	double *dist = (double *) malloc(sizeof(double)) * n * k);
-	int *cluster_assignment_cur = (int *) malloc(sizeof(int) * n);
-	int *cluster_assingment_prev = (int *) malloc(sizeof(int) * n);
-	double *point_move_score = (double *) malloc(sizeof(double) * n * k;
+	double *dist = (double *)malloc(sizeof(double) * n * k);
+	int *cluster_assignment_cur = (int *)malloc(sizeof(int) * n);
+	int *cluster_assignment_prev = (int *)malloc(sizeof(int) * n);
+	double *point_move_score = (double *)malloc(sizeof(double) * n * k);
 
 	if (!dist || !cluster_assignment_cur || !cluster_assignment_prev || !point_move_score)
-		fail("Error allocating distribution arrays");
+		cout << "Error allocating arrays. \n" << endl;
+
 	
 	// initial setup
-	calc_all_distances(dim, n, k, X, cluster_centroid, dist);
-	choose_all_clusters_from_distances(dim, n, k, dist, cluster_assignment_cur);
+	all_distances(dim, n, k, X, cluster_centroid, dist);
+	choose_all_clusters_from_distances(dim, n, k, dist, X, cluster_assignment_cur);
 	copy_assignment_array(n, cluster_assignment_cur, cluster_assignment_prev);
 
 	// batch update
@@ -312,13 +346,14 @@ void kmeans(int dim, double *X, int n, int k, double *cluster_centroid, int *clu
 	int batch_iteration = 0;
 	while (batch_iteration < I)
 	{
-		cout << "batch iteration " << batch_iteration << endl;
+		cout << "batch iteration \n" << batch_iteration << endl;
 		cluster_diag(dim, n, k, X, cluster_assignment_cur, cluster_centroid);
 		// update cluster centroids
 		calc_cluster_centroids(dim, n, k, X, cluster_assignment_cur, cluster_centroid);
 		// deal with empty clusters
 		// see if we've failed to improve
-		double totD = calc_total_distance(dim, n, k, X, cluster_assignment_cur, cluster_centroid);
+
+		double totD = calc_total_distance(dim, n, k, X, cluster_centroid, cluster_assignment_cur);
 		if (totD > prev_totD)
 			// failed to improve - this solution is worse than the last one
 			{
@@ -333,11 +368,11 @@ void kmeans(int dim, double *X, int n, int k, double *cluster_centroid, int *clu
 		// save previous step
 		copy_assignment_array(n, cluster_assignment_cur, cluster_assignment_prev);
 		// smoosh points around to nearest cluster
-		calc_all_distances(dim, n, k, X, cluster_centroid, dist);
-		choose_all_clusters_from_distances(dim, n, k, dist, cluster_assignment_cur);
+		all_distances(dim, n, k, X, cluster_centroid, dist);
+		choose_all_clusters_from_distances(dim, n, k, dist, X, cluster_assignment_cur);
 		int change_count = assignment_change_count(n, cluster_assignment_cur, cluster_assignment_prev);
-		cout << " " << batch_iteration << " " << change_count << " " << totD << " " << totD - prev_totD);
-		flush(stdout)
+		cout << " " << batch_iteration << " " << change_count << " \n" << totD << " \n" << (totD - prev_totD);
+		fflush(stdout);
 
 		// done with this phase if nothing has changed
 		if (change_count == 0)
@@ -364,63 +399,4 @@ Expect
 Max
 
 */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*************************************************************************************
-
-The aim of the approximation is as follows - given a collection of inner products, we want to estimate their distribution using a mixture of N weighted gaussians. We want to choose those gaussians that result in the closest fit to the true distribution of the inner products. This requires us to not only compute the means and variances of the N gaussians, but also to determine for each data point how responsible each of the gaussians was in generating it.
-
-	Beta is the weight
-	Mu is the mean
-	Sigma is the covariance
-	use Theta to represent the set of unknowns (beta, mu, sigma)
-
-However, in order to estimate these parameters, we will also need to determine how responsible each of our gaussians was in generating each data point.
-
-Supposing we have a data matrix U with K data vectors, each of which is D dimensional, we want to compute the matrix J, where each J i,j represents the 	probability that the ith data point was generated by the jth gaussian. We want to compute the value of theta which maximizes the log probability of U given theta. 
-
-Given an initial estimate of theta (theta0), we repeatedly perform two steps until the estimates of theta converge.
-
-************************************************************************************/
-
 
